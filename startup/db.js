9829d46a39774f602c8fa64c766c4fd66768d863
Sujish Pradeep <sujishp164@gmail.com>
2019-08-12 17:15:41 +1000

    db connection updated

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const db = config.get("db");
 
 module.exports = function() {
   mongoose
-    .connect("mongodb://localhost/trailo", { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("Could not connect", err));
   mongoose.set("useFindAndModify", false);
